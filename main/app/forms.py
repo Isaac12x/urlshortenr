04f90bc95635131f27c756bc3bb1e74589e5db54
Hunterx7 <ialbetsram@gmail.com>
2018-10-21 18:37:23 +0100

    Started recaptcha implementation and extended link models

--- main/app/forms.py
+++ main/app/forms.py
@@ -1,7 +1,9 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import StringField
 from wtforms.validators import DataRequired
 
 
 class ShortenUrlForm(FlaskForm):
+    # needs checking the passed data contains 'http or https or starts with www. app. or login. and nothign else'
     url = StringField('url', validators=[DataRequired()])
+    recaptcha = RecaptchaField()
--- main/config.py
+++ main/config.py
@@ -5,7 +5,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    TESTING=True
     #CSRF_ENABLED = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PORT = int(os.environ.get("PORT", 5000))
+    WTF_CSRF_ENABLED = True if os.environ.get("DEBUG") == False else False
+    WTF_CSRF_SECRET_KEY = 'KVVLRCPGAOABCISAZGCMCBTHOZZBEELZYSGHSXXY99TSEMMFZSVIFWSGGOWAAXSJINRWSHQDEXLASWCCU'
+    RECAPTCHA_PUBLIC_KEY='6LcMkHUUAAAAAAFUQSfgVcpBPADCXNmOg0YO5lwT'
+    RECAPTCHA_PRIVATE_KEY='6LcMkHUUAAAAABgZ79x_38RQp_zsXrNz6qqWJZc0'
+    RECAPTCHA_API_SERVER=''
+    RECAPTCHA_PARAMETERS = {'render': 'explicit', 'data-size': "invisible"}
+    RECAPTCHA_DATA_ATTRS = {'theme': 'light'}
